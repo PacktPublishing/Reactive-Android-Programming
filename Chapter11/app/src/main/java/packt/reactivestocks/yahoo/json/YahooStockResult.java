048b4bdd0ef5438348312bc092fc40c6fff77620
rashilspackt <rashils@packtpub.com>
2017-05-24 19:06:18 +0530

    Code files

--- Chapter11/app/src/main/java/packt/reactivestocks/yahoo/json/YahooStockResult.java
+++
@@ -1,9 +0,0 @@
-package packt.reactivestocks.yahoo.json;
-
-public class YahooStockResult {
-    private YahooStockQuery query;
-
-    public YahooStockQuery getQuery() {
-        return query;
-    }
-}
